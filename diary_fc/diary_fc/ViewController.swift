f05fde987b874a8da2d4a80f3fc07109db6b496c
LeeHyeonHo-127 <84439622+LeeHyeonHo-127@users.noreply.github.com>
2023-03-16 23:00:08 +0900

    Start Clone Diary App

---
+++ diary_fc/diary_fc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diary_fc
+//
+//  Created by 이현호 on 2023/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
