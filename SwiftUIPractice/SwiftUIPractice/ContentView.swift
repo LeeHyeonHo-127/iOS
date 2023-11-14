5cf53f1acc18e3c9625adb8450ff40c39d19db1a
LeeHyeonHo-127 <swethom1@naver.com>
2023-11-14 17:17:50 +0900

    swiftUI

--- SwiftUIPractice/SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -8,22 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    let name: String = "hello"
+    
     var body: some View {
-        HStack(
-            alignment: .top,
-            spacing: 10
-        ) {
-            ForEach(
-                1...5,
-                id: \.self
-            ) {
-                Text("Item \($0)")
-            }
+        HStack {
+            Image(systemName: "checkmark")
+            Spacer()
+            Text(name)
         }
-   }
-
+        .border(Color.gray)
+    }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+//TapView 연습
+/*
+struct ContentView: View {
+    
+    var body: some View {
+        TabView {
+            Text("The First Tap")
+                .badge(2)
+                .tabItem{
+                    Label("Received", systemImage: "tray.and.arrow.down.fill")
+                }
+            Text("Another Tab")
+                .tabItem{
+                    Label("Sent", systemImage: "tray.and.arrow.up.fill")
+                }
+            Text("The Last Tab")
+                .badge("!")
+                .tabItem {
+                    Label("Account", systemImage: "person.crop.circle.fill")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+*/
--- SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct SwiftUIPracticeApp: App {
+    
+    
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
